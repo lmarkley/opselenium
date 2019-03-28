c8aaf6581a818603699c5351c9981251a24fe06b
Larry Markley <lmarkley12@gmail.com>
2019-03-28 18:40:36 -0500

    updated with more exception considerations

--- opselenium.py
+++ opselenium.py
@@ -3,9 +3,18 @@ from selenium.webdriver.common.keys import Keys
 import datetime
 
 todayDate = datetime.datetime.now().strftime("%m/%d/%y");
-siteID = ['SID0',
-'SID1',
-'SID2']
+siteID = [
+'S********',
+'S********',
+'S********',
+'S********',
+'S********',
+'S********',
+'S********',
+'S********',
+'S********',
+'S********'
+]
 
 # set browser option to run 'headless'
 options = webdriver.ChromeOptions()
@@ -15,7 +24,7 @@ options.add_argument('--headless')
 # be in the script directory or on the system path.)
 browser = webdriver.Chrome(options=options)
 
-browser.get('INSERT OPS')
+browser.get('INSERT OPS URL')
 old_window_handle = browser.current_window_handle
 
 # log ourselves in!
@@ -27,7 +36,7 @@ pass_elem.send_keys('INSERT PASS' + Keys.RETURN)
 
 for sindex in siteID:
     
-    browser.execute_script("window.open('INSERT OPS')")
+    browser.execute_script("window.open('INSERT OPS URL')")
 
     new_window_handle = [window for window in browser.window_handles if window != old_window_handle][0]
     
@@ -43,25 +52,36 @@ for sindex in siteID:
     idSearch_elem = browser.find_element_by_name('idSearch')
     idSearch_elem.send_keys(sindex + Keys.RETURN)
     
+    commType_elem = browser.find_element_by_xpath('//*[@id="siteInfo"]/div/div[2]/div[2]/div[2]/div/span[2]').text
+
     deviceInfo_elem = browser.find_element_by_xpath('//*[@id="siteDetails"]/div[4]/div[6]/div[2]/div[3]/span[2]').text
-    lastCheckin_elem = browser.find_element_by_id('lastCheckin').text
-    lastAuth_elem = browser.find_element_by_class_name('siteData').text
-    if '(yesterday)' in deviceInfo_elem: 
-        print('Site reported data yesterday, may be online. Checking "Last Checkin" time...')
-        if todayDate in lastCheckin_elem:
-                print('Site %s is online, check for data.' % sindex)
-        else:
-            print('Site %s is not online.' % sindex)
-    else: 
-        print('Site has not reported data recently and may be offline. Checking "Last Checkin" time...')
-        if todayDate in lastCheckin_elem:
-            if todayDate in lastAuth_elem:
-                print('The lead device that the site is checking in. Check Orbit for an OOS flag or a connection issue.')
-            else: 
-                print('The lead device is checking in but has not been authorized.')
-        else:
-            print('Site %s is not online.' % sindex)
-    
+    if commType_elem == 'SSC':
+        lastCheckin_elem = browser.find_element_by_id('lastCheckin').text
+        lastAuth_elem = browser.find_element_by_class_name('siteData').text
+
+        if '(yesterday)' in deviceInfo_elem: 
+            print('Site reported data yesterday, may be online. Checking "Last Checkin" time...')
+            
+            if todayDate in lastCheckin_elem:
+                    print('Site %s is online, check for data.' % sindex)
+            else:
+                print('Site %s is not online.' % sindex)
+            
+        else: 
+            print('Site has not reported data recently and may be offline. Checking "Last Checkin" time...')
+            if commType_elem == 'SSC':
+                if todayDate in lastCheckin_elem:
+                    if todayDate in lastAuth_elem:
+                        print('The lead device that the site is checking in. Check Orbit for an OOS flag or a connection issue.')
+                    else: 
+                        print('The lead device is checking in but has not been authorized.')
+                else:
+                    print('Site %s is not online.' % sindex)
+            else:
+                print('Communication type does not support "Checkin" time checks. Check if site %s is online manually.' % sindex)
+    else:
+        print('Communication type does not support "Checkin" time checks. Check if site %s is online manually.' % sindex)
+
     if browser.current_window_handle == old_window_handle:    
         browser.close()
         browser.switch_to.window(new_window_handle)
