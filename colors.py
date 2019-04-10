c75c3fa138cfed624ed97527515c4037fe404956
Larry Markley <lmarkley12@gmail.com>
2019-04-10 16:11:23 -0500

    Create colors.py

---
+++ colors.py
@@ -0,0 +1,11 @@
+#  -- colors.py --
+# adapted from [this](https://stackoverflow.com/a/37340245) StackOverflow answer.
+# for use with colorama
+RED   	= "\033[1;31m"  
+BLUE  	= "\033[1;34m"
+CYAN  	= "\033[1;36m"
+GREEN 	= "\033[0;32m"
+YELLOW 	= "\033[0:93m"
+RESET 	= "\033[0;0m"
+BOLD    = "\033[;1m"
+REVERSE = "\033[;7m"
